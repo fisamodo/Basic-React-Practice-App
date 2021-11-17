bbfa00adbbce0567ccf54a24627ea16268f04d1f
Filip Velebit Samodol <fisamodo@vsite.hr>
2021-11-17 16:29:34 +0100

    REACT-0001 - add button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
-const Header = () => {
-    return (
-        <header>
-            <h1>Task Tracker</h1>
-        </header>
-    )
-}
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-export default Header
+const Header = ({ title }) => {
+  const onClick = (e) => {
+    console.log("asd");
+  };
+
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+/*const headingStyle = {
+    color: 'red'
+}*/
+
+export default Header;
