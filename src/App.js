9834795a446e4a5a57b12336cdd158ff2ea863fa
Filip Velebit Samodol <fisamodo@vsite.hr>
2021-11-17 17:05:41 +0100

    REACT-0004 add reminder functionality

--- src/App.js
+++ src/App.js
@@ -24,16 +24,28 @@ function App() {
     },
   ]);
 
-//Delete Task
-const deleteTask = (id) => {
-  setTasks(tasks.filter((task) => task.id !== id))
-}
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
       <Header />
-      { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} />
-      : "No Tasks to Show"}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks to Show"
+      )}
     </div>
   );
 }
