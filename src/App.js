79972772157b4bb8965700312d941437933d8904
Filip Velebit Samodol <fisamodo@vsite.hr>
2021-11-17 16:50:11 +0100

    REACT-0002 - Make a task component, install icons from npm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import Header from './components/Header'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Football Appointment",
+      day: "Feb 9th",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Tennis Appointment",
+      day: "Feb 4th",
+      reminder: false,
+    },
+  ]);
+
+//Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
   return (
     <div className="container">
       <Header />
+      { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} />
+      : "No Tasks to Show"}
     </div>
   );
 }
